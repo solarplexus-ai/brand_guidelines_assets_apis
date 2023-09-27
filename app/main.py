96c083309ce60dee3d498090b557b96b01c2948a
Prashant1729 <vijaykambi3@gmail.com>
2023-09-27 17:01:20 +0530

    first commit

---
+++ app/main.py
@@ -0,0 +1,54 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello from FastAPI!"}
+
+
+import bcrypt
+from fastapi import FastAPI
+from app.models import User
+from db.supabase import create_supabase_client
+
+app = FastAPI()
+
+# Initialize supabase client
+supabase = create_supabase_client()
+
+def user_exists(key: str = "email", value: str = None):
+    user = supabase.from_("users").select("*").eq(key, value).execute()
+    return len(user.data) > 0
+
+# Create a new user
+@app.post("/user")
+def create_user(user: User):
+    try:
+        # Convert email to lowercase
+        user_email = user.email.lower()
+        # Hash password
+        hased_password = bcrypt.hashpw(user.password, bcrypt.gensalt())
+
+        # Check if user already exists
+        if user_exists(value=user_email):
+            return {"message": "User already exists"}
+
+        # Add user to users table
+        user = supabase.from_("users")\
+            .insert({"name": user.name, "email": user_email, "password": hased_password})\
+            .execute()
+
+        # Check if user was added
+        if user:
+            return {"message": "User created successfully"}
+        else:
+            return {"message": "User creation failed"}
+    except Exception as e:
+        print("Error: ", e)
+        return {"message": "User creation failed"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ config.py
@@ -0,0 +1,2 @@
+URL = "https://zeqeckzogqdxaugnhnqa.supabase.co"
+api = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplcWVja3pvZ3FkeGF1Z25obnFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU3MjgxNTMsImV4cCI6MjAxMTMwNDE1M30.A_8F1DysbYYu7ydeWuR7yAKKIL_8Ir3jeDz-mBQDSGk"
